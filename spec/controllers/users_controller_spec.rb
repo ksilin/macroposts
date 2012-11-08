78171b13b38e03ca2078266cec070d7652c34afc
kostja <konstantin@silin.info>
2012-11-08 18:53:06 +0100

    signup title test

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,11 +2,22 @@ require 'spec_helper'
 
 describe UsersController do
 
+  #if not called, the specs call the ctrlr actions, but do not render the views
+  # it's a trade-off between execution speed and missing some errors in the views
+  render_views
+
+
   describe "GET 'new'" do
     it "returns http success" do
       get 'new'
       response.should be_success
     end
+  end
+
+  it "should have the right title" do
+    get 'new'
+    response.should have_selector('title', :content => "Sign Up")
+
   end
 
 end
